79e9d7ebccf1c3d6f8782261537ac036557bdaad
Zack Forbing <zackforbing@gmail.com>
2016-07-03 19:51:45 -0600

    all tests passing

--- test/features/user_sees_robots_test.rb
+++ test/features/user_sees_robots_test.rb
@@ -31,21 +31,16 @@ class UserSeesRobotsTest < FeatureTest
       assert page.has_content?("George Marshall")
     end
 
-    # visit "robots/#{current_robot_id - 1}"
-    #
-    # assert page.has_content?("Caligula")
-    # assert page.has_content?("city: Fallujah")
-    # assert page.has_content?("state: Haram")
-    # assert page.has_content?("birthdate: June 11, 1945")
-    # assert page.has_content?("date hired: yesterday")
-    # assert page.has_content?("department: Grocery Bagger")
+    visit "robots/#{current_robot_id - 1}"
 
-    #both robots show up on the /robots page, but when I save_and_open_page with
-    #George Marshall, all I get is a message saying "Not Found". the capybara test
-    #page shows that George Marshall does have a link, and prying shows that my usage
-    #of current_robot_id is correct...not sure why this fails.
-    #also, George Marshall's page shows up just fine when I comment out the first one??
-    #so the test runs like this...but uncomment the above code and it fails. 
+    assert page.has_content?("Caligula")
+    assert page.has_content?("city: Fallujah")
+    assert page.has_content?("state: Haram")
+    assert page.has_content?("birthdate: June 11, 1945")
+    assert page.has_content?("date hired: yesterday")
+    assert page.has_content?("department: Grocery Bagger")
+
+    visit "/robots"
     visit "robots/#{current_robot_id}"
 
     assert page.has_content?("George Marshall")
