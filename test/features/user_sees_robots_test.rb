520b3365b3ffd0e8b46e74efc28e5f9ec4b279b9
Zack Forbing <zackforbing@gmail.com>
2016-07-03 18:39:55 -0600

    feature tests pass

--- test/features/user_sees_robots_test.rb
+++ test/features/user_sees_robots_test.rb
@@ -2,20 +2,27 @@ require_relative '../test_helper'
 
 class UserSeesRobotsTest < FeatureTest
 
-  def test_user_sees_robo
-    # As a user, when I visit the tasks index, then I should see a list of my tasks.
-    task1 = task_manager.create({
-      title: "Coding forever",
-      description: "never stop"
+  def test_user_sees_robot
+    robot_world.create({
+      name: "Caligula",
+      city: "Fallujah",
+      state: "Haram",
+      birthdate: "June 11, 1945",
+      date_hired: "yesterday",
+      department: "Grocery Bagger"
       })
-    task1 = task_manager.create({
-      title: "Learn Capybara",
-      description: "hot tub james brown"
+    robot_world.create({
+      name: "George Marshall",
+      city: "New New York",
+      state: "Earth",
+      birthdate: "June 11, 1967",
+      date_hired: "yesterday",
+      department: "Florist"
       })
-    visit "/tasks"
-    within("#task_list") do
-      assert page.has_content?("Coding forever")
-      assert page.has_content?("Learn Capybara")
+    visit "/robots"
+    within("#robot_list") do
+      assert page.has_content?("Caligula")
+      assert page.has_content?("George Marshall")
     end
   end
 end
